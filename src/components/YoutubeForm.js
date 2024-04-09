26f52e0a835e851d6dadffc00ecf9187d53c851c
Naiem YF <iritman@gmail.com>
2024-04-10 00:07:55 +0330

    Field component

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form } from "formik";
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
 const YoutubeForm = () => {
@@ -28,15 +28,7 @@ const YoutubeForm = () => {
       <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            {...formik.getFieldProps("name")}
-            // onChange={formik.handleChange}
-            // onBlur={formik.handleBlur}
-            // value={formik.values.name}
-          />
+          <Field type="text" id="name" name="name" />
           {formik.touched.name && formik.errors.name && (
             <div className="error">{formik.errors.name}</div>
           )}
@@ -44,15 +36,7 @@ const YoutubeForm = () => {
 
         <div className="form-control">
           <label htmlFor="email">E-mail</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            {...formik.getFieldProps("email")}
-            // onChange={formik.handleChange}
-            // onBlur={formik.handleBlur}
-            // value={formik.values.email}
-          />
+          <Field type="email" id="email" name="email" />
           {formik.touched.email && formik.errors.email && (
             <div className="error">{formik.errors.email}</div>
           )}
@@ -60,15 +44,7 @@ const YoutubeForm = () => {
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <input
-            type="text"
-            id="channel"
-            name="channel"
-            {...formik.getFieldProps("channel")}
-            // onChange={formik.handleChange}
-            // onBlur={formik.handleBlur}
-            // value={formik.values.channel}
-          />
+          <Field type="text" id="channel" name="channel" />
           {formik.touched.channel && formik.errors.channel && (
             <div className="error">{formik.errors.channel}</div>
           )}
